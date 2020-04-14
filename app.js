1bdc6bf4e35588a5ccb4f1812534993004e463bd
brandontgormley@gmail.com <brandontgormley@gmail.com>
2020-04-14 12:25:25 -0400

    Added starter JS
    -Added some selectors
    -Created Phrases Array
    -Clicking start removes overlay to display game

---
+++ app.js
@@ -0,0 +1,25 @@
+// Selectors
+
+const qwerty = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase');
+const overlay = document.querySelector('#overlay');
+const startButton = document.querySelector('.btn__reset');
+
+// Missed Attempts
+let missed = 0;
+
+// Phrase Array
+const phrases = ['Success is the way of life', 'If you believe it, you can achieve it', 'Do not give up', 'Failure sets up victory', 'Patience will unlock the key to success'];
+
+// Start Game
+startButton.addEventListener('click', () => {
+    overlay.style.display = 'none';
+});
+
+// Selectors a random Phrase from array
+function getRandomPhraseAsArray(arr) {
+    let randomPhrase = arr[Math.floor(Math.random() * arr.length)];
+    console.log(randomPhrase);
+}
+
+// Added the Selected Phrase to the display
